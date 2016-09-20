ce716b4cbf89812bec5fce31de8a0ebee74fd576
Alec Faithfull <alef@itu.dk>
2016-09-20 10:10:54 +0200

    Added a skeleton Eclipse plugin

---
+++ plugins/dk.itu.coqoon.opam/src/dk/itu/coqoon/opam/Activator.scala
@@ -0,0 +1,21 @@
+package dk.itu.coqoon.opam
+
+import org.osgi.framework.BundleContext
+import org.eclipse.ui.plugin.AbstractUIPlugin
+
+class Activator extends AbstractUIPlugin {
+  override def start(context : BundleContext) = {
+    super.start(context)
+    Activator.instance = this
+  }
+
+  override def stop(context : BundleContext) = {
+    Activator.instance = null
+    super.stop(context)
+  }
+}
+object Activator {
+  private var instance : Activator = _
+
+  def getDefault() = instance
+}
