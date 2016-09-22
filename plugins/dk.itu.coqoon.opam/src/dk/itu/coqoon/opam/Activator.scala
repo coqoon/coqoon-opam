71ebab931505e49e1fb2911209781401bced95da
Alec Faithfull <alef@itu.dk>
2016-09-22 13:17:10 +0200

    Override Coqoon's Coq path to the active OPAM root's Coq installation

--- plugins/dk.itu.coqoon.opam/src/dk/itu/coqoon/opam/Activator.scala
+++ plugins/dk.itu.coqoon.opam/src/dk/itu/coqoon/opam/Activator.scala
@@ -3,6 +3,9 @@ package dk.itu.coqoon.opam
 import org.osgi.framework.BundleContext
 import org.eclipse.ui.plugin.AbstractUIPlugin
 
+import dk.itu.coqoon.core.ICoqPathOverride
+import dk.itu.coqoon.opam.ui.OPAMPreferences
+
 class Activator extends AbstractUIPlugin {
   override def start(context : BundleContext) = {
     super.start(context)
@@ -18,4 +21,14 @@ object Activator {
   private var instance : Activator = _
 
   def getDefault() = instance
+}
+
+class Override extends ICoqPathOverride {
+  import org.eclipse.core.runtime.{Path, IPath}
+  override def getCoqPath() = {
+    val path = OPAMPreferences.ActiveRoot.get
+    path.map(p => new Path(
+        OPAM.canonicalise(new Path(p)).getPackage("coq").getConfigVar("bin"))
+    ).getOrElse(Path.EMPTY)
+  }
 }
